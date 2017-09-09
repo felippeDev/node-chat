80b8c9522be95e92b723512078f532b95d9447fb
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-09-09 14:11:20 -0300

    host config

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = (application) => {
     application.get('/', (req, res) => {
+        console.log(application.appPath); //test
         application.controllers.index.home(application, req, res);
     })
 }
