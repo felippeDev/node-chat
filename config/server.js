41297588c75296dfc61bb5bcc2193838265d6164
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-09-09 13:31:45 -0300

    host config

--- config/server.js
+++ config/server.js
@@ -2,18 +2,20 @@ const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
+app.set('views', appDir + '/views');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(appDir + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
 consign()
-    .include(__dirname + '/routes')
+    .include(appDir + '/routes')
     .then('/models')
     .then('/controllers')
     .into(app);
