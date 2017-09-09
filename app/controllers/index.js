389ef8433f98a04f15f324a3a63701782fb753a1
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-09-09 15:35:04 -0300

    host config

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,8 +1,3 @@
-const path = require('path');
-const appDir = path.dirname(require.main.filename);
-
 module.exports.home = (application, req, res) => {
-    console.log(appDir);
-    console.log(application.appDir);
-    res.render(appDir + 'index', { validationErrors: {} });
+    res.render(application.appDir + 'index', { validationErrors: {} });
 }
