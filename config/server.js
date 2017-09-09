0f432db66219e09262f2f8d0b5b7c9682735b523
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-09-09 12:11:58 -0300

    host config

--- config/server.js
+++ config/server.js
@@ -8,14 +8,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/app/views');
 
-app.use(express.static(__dirname + '/app/public'));
+app.use(express.static(__dirname + './app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
 consign()
-    .include(__dirname + '/app/routes')
-    .then(__dirname + '/app/models')
-    .then(__dirname + '/app/controllers')
+    .include('./app/routes')
+    .then('./app/models')
+    .then('./app/controllers')
     .into(app);
 
 module.exports = app;
