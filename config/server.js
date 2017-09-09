4f65bb54c136b14ae8d9dea75ede779b3edca00a
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-09-09 20:17:58 -0300

    host config - reverted

--- config/server.js
+++ config/server.js
@@ -2,15 +2,13 @@ const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
-// const path = require('path');
-// const appPath = path.dirname(require.main.filename);
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
-app.use(express.static('./app/public'));
+app.use(express.static('app/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 
